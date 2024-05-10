b2483a2bbc1f5c89478780293b661bf56decc7df
user <ameyspero@gmail.com>
2024-05-10 23:11:15 +0100

    checking

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ExampleController;
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\ExampleController;
+    use Illuminate\Support\Facades\Route;
 
-Route::get('/',  [ExampleController::class, 'index'])->name('home');
+    Route::get('/',  [ExampleController::class, 'index'])->name('home');
