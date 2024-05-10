89d6a97e7b7ad9f3de1c7a91fb2ef56cce7f2663
user <ameyspero@gmail.com>
2024-05-10 20:54:53 +0100

    Adding Quote model

---
+++ app/Models/Quote.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Quote extends Model
+{
+    use HasFactory;
+    protected $fillable = ['id','site_id', 'destination_id', 'date_quoted'];
+    protected $table = 'quotes';
+
+    public static function renderHtml(Quote $quote)
+    {
+        return '<p>' . $quote->id . '</p>';
+    }
+
+    public static function renderText(Quote $quote)
+    {
+        return (string) $quote->id;
+    }
+}
